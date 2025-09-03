f961e301880755d629a1135870fbc313a4ff8c9c
Karolis K. <krk217@protonmail.com>
2025-09-03 13:34:27 +0300

    2125D, 2134E

--- Codeforces/2125/D.cpp
+++ Codeforces/2125/D.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 using ll = long long;
+using segment = array<int, 4>;
 
 constexpr int mod = 998244353;
 
@@ -14,14 +15,32 @@ int toPower(int a, int p) {
     return res;
 }
 
+int Inv(int a) { return toPower(a, mod - 2); }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, m;
     cin >> n >> m;
+    vector<segment> seq;
     while (n--) {
         int l, r, p, q;
+        cin >> l >> r >> p >> q;
+        int prob = static_cast<ll>(p) * Inv(q) % mod;
+        seq.push_back({l, r, prob});
     }
+    ranges::sort(seq);
+    int globalMult = 1;
+    vector cur(m + 2, 0);
+    cur[1] = 1;
+    for (auto& s : seq) {
+        int my = static_cast<ll>(globalMult) * cur[s[0]] % mod;
+        globalMult = static_cast<ll>(globalMult) * (1 - s[2] + mod) % mod;
+        int add = static_cast<ll>(my) * s[2] % mod * Inv(globalMult) % mod;
+        cur[s[1] + 1] = (cur[s[1] + 1] + add) % mod;
+    }
+    int ans = static_cast<ll>(globalMult) * cur[m + 1] % mod;
+    cout << ans << "\n";
     return 0;
 }
--- Codeforces/2134/E.cpp
+++ Codeforces/2134/E.cpp
@@ -1,9 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ii = pair<int, int>;
+
+void Swap(int x, vector<int>& seq) {
+    swap(seq[x], seq[x + 1]);
+    cout << "swap " << x + 1 << endl;
+}
+
+int Throw(int x) {
+    cout << "throw " << x + 1 << endl;
+    int res;
+    cin >> res;
+    return res;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> seq(n);
+        iota(seq.begin(), seq.end(), 0);
+        vector<int> res(n);
+        int a = 0, b = 0;
+        for (int i = n - 1; i > 0; i--)
+            if (a == b) {
+                res[seq[i - 1]] = Throw(i - 1) == a + 2? 1: 2;
+                Swap(i - 1, seq);
+                a++;
+            } else if (Throw(i) == a + 1) {
+                res[seq[i]] = 1;
+                a++; b++;
+            } else {
+                res[seq[i]] = 2;
+                b++;
+            }
+        if (a == b) {
+            Swap(0, seq);
+            res[seq[1]] = Throw(1) == a + 1? 1: 2;
+        } else res[seq[0]] = Throw(0) == a + 1? 1: 2;
+        cout << "!";
+        for (auto& x : res)
+            cout << " " << x;
+        cout << endl;
+    }
     return 0;
 }
