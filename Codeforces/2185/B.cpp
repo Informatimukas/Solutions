edab867b101e4a64a5ee6d331911682b1767ac9f
Karolis K. <krk217@protonmail.com>
2026-02-06 23:33:50 +0400

    2184A, 2185B.

---
+++ Codeforces/2185/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto& x : a)
+            cin >> x;
+        ranges::sort(a);
+        cout << n * a[n - 1] << "\n";
+    }
+    return 0;
+}
